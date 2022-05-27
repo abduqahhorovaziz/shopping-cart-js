9ecd57864f4a27a3f36201779964ea084bde6205
abduqahhorovaziz <83882515+abduqahhorovaziz@users.noreply.github.com>
2022-05-27 17:28:00 +0500

    the add to cart button worked

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
-let countNumb = document.querySelector("count");
-let btn = document.getElementById('btn');
+let btn = document.querySelectorAll(".cart-add-btn");
 
-btn.addEventListener("click", addProduct);
-
-function addProduct(){
-    console.log("click")
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", () => {
+    cartNumbers();
+  });
 }
+
+function onLoadCartNumbers() {
+  let productNumbers = localStorage.getItem("cartNumbers");
+
+  if (productNumbers) {
+    document.querySelector(".count").textContent = productNumbers;
+  }
+}
+
+function cartNumbers() {
+  let productNumbers = localStorage.getItem("cartNumbers");
+  productNumbers = parseInt(productNumbers);
+
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    document.querySelector(".count").textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem("cartNumbers", 1);
+    document.querySelector(".count").textContent = 1;
+  }
+}
+
+
+onLoadCartNumbers();
