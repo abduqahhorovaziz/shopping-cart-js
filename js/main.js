9c57e9947fa75989f95803a146b5d8b133af9a4b
abduqahhorovaziz <83882515+abduqahhorovaziz@users.noreply.github.com>
2022-05-27 14:38:15 +0500

    the javascript side remained

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let countNumb = document.querySelector("count");
+let btn = document.getElementById('btn');
+
+btn.addEventListener("click", addProduct);
+
+function addProduct(){
+    console.log("click")
+}
